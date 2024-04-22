a7e74d2c41b93d05caf6b29a1b86a7e116281d08
Alexatawesomeaj <141694427+Alexatawesomeaj@users.noreply.github.com>
2024-04-22 09:38:27 -0600

    Added variables for all of the values, to let move

--- Week 10/LiveSite/style.js
+++ Week 10/LiveSite/style.js
@@ -1,9 +1,74 @@
+//details of Title Text
+  var titleX = 0;
+  var titleY = 24;
+  var size = 24;
+//location of Author Text
+  var authX = 165;
+  var authy = 160;
+//general arm details
+  var armSize = 9;
+  var armColorR = 235;
+  var armColorG = 187;
+  var armColorB = 129;
+//right arm pos
+  var rArmPosX1 = 220;
+  var rArmPosY1 = 40;
+  var rArmPosX2 = 240;
+  var rArmPosY2 = 5;
+//left arm pos
+  var lArmPosX1 = 179;
+  var lArmPosY1 = 54;
+  var lArmPosX2 = 165;
+  var lArmPosY2 = 80;
+//head details
+  var headR = 235;
+  var headG = 187;
+  var headB = 129;
+  var headX = 199;
+  var headY = 22;
+  var headD = 36;
+//shirt color
+  var shirtR = 36;
+  var shirtG = 96;
+  var shirtB = 191;
+//chest pos
+  var chestX1 = 186;
+  var chestY1 = 40;
+  var chestX2 = 27;
+  var chestY2 = 52;
+//left sleeve
+  var lSleeveX1 = 186;
+  var lSleeveY1 = 40;
+  var lSleeveX2 = 186;
+  var lSleeveY2 = 53;
+  var lSleeveX3 = 171;
+  var lSleeveY3 = 53;
+//right sleeve
+  var rSleeveX1 = 213;
+  var rSleeveY1 = 40;
+  var rSleeveX2 = 213;
+  var rSleeveY2 = 53;
+  var rSleeveX3 = 229;
+  var rSleeveY3 = 40;
+//pants color
+  var pantR = 1;
+  var pangG = 5;
+  var pantB = 15;
+
+
+function sleep(ms) {
+  //Borrowed code to implement a delay (sleep) function
+  return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
 function setup() {
-    createCanvas(400, 400);
+  //Generates a canvas to draw on top of
+    createCanvas(800, 1200);
   }
   
   
   function draw() {
+    //Generates a color for canvas (clear), and the text on it
     clear();
     textSize(24);
     fill(255);
@@ -39,6 +104,7 @@ function setup() {
     fill(1, 5, 15);
     triangle(178, 130, 198, 92, 190, 135);
     triangle(178, 130, 186.5, 92, 199, 92);
+    //quad(178, 130) *TODO
     
     //right leg
     triangle(200, 92, 202, 135, 214, 92);
